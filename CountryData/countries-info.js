bf7454cf2342fad98929a8564b020471ec9d87c5
Nikolay Prodanow <nikolayprodanow@gmail.com>
2016-11-26 15:36:45 +0200

    add empty json for country info

---
+++ CountryData/countries-info.js
@@ -0,0 +1,14 @@
+[
+    {
+        name: "",
+        countryURL: "",
+        capital: "",
+        currency: "",
+        population: 0,
+        area: 0,
+        populationDensity: 0,
+        latitude: "",
+        longitude: "",
+        countryInformation: ""
+    }
+]
