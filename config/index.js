eabaf5aa33b0d8adb22b4323fda2c685159d0ea0
Mariana Naidenova <mariananaidenova@mail.bg>
2016-11-28 23:16:52 +0200

    add local connection string and questions css fixes

--- config/index.js
+++ config/index.js
@@ -3,6 +3,7 @@
 let port = process.env.PORT || 3001;
 
 module.exports = {
+    // connectionString: "mongodb://localhost/mappyDb",
     connectionString: "mongodb://heroku_96vdp9gb:d6h68kum027hn1h42n6d2c5o5m@ds113938.mlab.com:13938/heroku_96vdp9gb",
     port: port
 };
