765c7e0979180c97b4b4eaa63428e1384892f8c6
Nikolay Prodanow <nikolayprodanow@gmail.com>
2016-11-28 13:19:16 +0200

    add autocomplete in admin panel search

---
+++ public/js/admin-panel.js
@@ -0,0 +1,8 @@
+/* globals $ requester */
+
+$("#search-input").on("click", function (ev) {
+    requester.getJSON("/api/users")
+        .then(users => {
+            $(ev.target).autocomplete({ source: users });
+        });
+});
