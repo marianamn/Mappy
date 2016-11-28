f82a8b7d2b714aaf6d9bcef5232af3abfb07135a
Nikolay Prodanow <nikolayprodanow@gmail.com>
2016-11-28 07:52:09 +0200

    add basic questions for nikolay countries

---
+++ CountryData/questions-nikolay.js
@@ -0,0 +1,772 @@
+[
+    {
+        question: "What is the capital of Ukraine?",
+        answers: [
+            {
+                answer: "Kyiv",
+                isCorrect: true
+            },
+            {
+                answer: "Sofia",
+                isCorrect: false
+            },
+            {
+                answer: "Luanda",
+                isCorrect: false
+            },
+            {
+                answer: "Tbilisi,",
+                isCorrect: false
+            }
+        ],
+        country: "Ukraine"
+    },
+    {
+        question: "What is the official  currency of Ukraine?",
+        answers: [
+            {
+                answer: "Ukrainian hryvnia",
+                isCorrect: true
+            },
+            {
+                answer: "Euro",
+                isCorrect: false
+            },
+            {
+                answer: "Danish krone",
+                isCorrect: false
+            },
+            {
+                answer: "Ukrainian rekyat",
+                isCorrect: false
+            }
+        ],
+        country: "Ukraine"
+    },
+    {
+        question: "What is the population of Ukraine?",
+        answers: [
+            {
+                answer: "46480700",
+                isCorrect: true
+            },
+            {
+                answer: "42600200",
+                isCorrect: false
+            },
+            {
+                answer: "63340458",
+                isCorrect: false
+            },
+            {
+                answer: "12459576",
+                isCorrect: false
+            }
+        ],
+        country: "Ukraine"
+    },
+    {
+        question: "What is the area of Ukraine (km2)?",
+        answers: [
+            {
+                answer: "603700",
+                isCorrect: true
+            },
+            {
+                answer: "560231",
+                isCorrect: false
+            },
+            {
+                answer: "341984",
+                isCorrect: false
+            },
+            {
+                answer: "503495",
+                isCorrect: false
+            }
+        ],
+        country: "Ukraine"
+    },
+    {
+        question: "What is the population density of Ukraine (people per km2 of land area)?",
+        answers: [
+            {
+                answer: "77",
+                isCorrect: true
+            },
+            {
+                answer: "23",
+                isCorrect: false
+            },
+            {
+                answer: "103",
+                isCorrect: false
+            },
+            {
+                answer: "<1",
+                isCorrect: false
+            }
+        ],
+        country: "Ukraine"
+    },
+    {
+        question: "What is the capital of Turkey?",
+        answers: [
+            {
+                answer: "Ankara",
+                isCorrect: true
+            },
+            {
+                answer: "Asmara",
+                isCorrect: false
+            },
+            {
+                answer: "Tehran",
+                isCorrect: false
+            },
+            {
+                answer: "Reykjavik",
+                isCorrect: false
+            }
+        ],
+        country: "Turkey"
+    },
+    {
+        question: "What is the population of Turkey?",
+        answers: [
+            {
+                answer: "73192840",
+                isCorrect: true
+            },
+            {
+                answer: "75092840",
+                isCorrect: false
+            },
+            {
+                answer: "84192840",
+                isCorrect: false
+            },
+            {
+                answer: "62192840",
+                isCorrect: false
+            }
+        ],
+        country: "Turkey"
+    },
+    {
+        question: "What is the official currency of Turkey?",
+        answers: [
+            {
+                answer: "Turkish new lira",
+                isCorrect: true
+            },
+            {
+                answer: "Turkish old lira",
+                isCorrect: false
+            },
+            {
+                answer: "United States dollar",
+                isCorrect: false
+            },
+            {
+                answer: "Euro",
+                isCorrect: false
+            }
+        ],
+        country: "Turkey"
+    },
+    {
+        question: "What is the area of Turkey (km2)?",
+        answers: [
+            {
+                answer: "783562",
+                isCorrect: true
+            },
+            {
+                answer: "650562",
+                isCorrect: false
+            },
+            {
+                answer: "792310",
+                isCorrect: false
+            },
+            {
+                answer: "451023",
+                isCorrect: false
+            }
+        ],
+        country: "Turkey"
+    },
+    {
+        question: "What is the population density of Turkey (people per km2)?",
+        answers: [
+            {
+                answer: "93",
+                isCorrect: true
+            },
+            {
+                answer: "150",
+                isCorrect: false
+            },
+            {
+                answer: "3",
+                isCorrect: false
+            },
+            {
+                answer: "45",
+                isCorrect: false
+            }
+        ],
+        country: "Turkey"
+    },
+    {
+        question: "what is the capital of Sweden?",
+        answers: [
+            {
+                answer: "Stockholm",
+                isCorrect: true
+            },
+            {
+                answer: "Riga",
+                isCorrect: false
+            },
+            {
+                answer: "Luxembourg",
+                isCorrect: false
+            },
+            {
+                answer: "Bern",
+                isCorrect: false
+            }
+        ],
+        country: "Sweden"
+    },
+    {
+        question: "What is the area of Sweden (km2)?",
+        answers: [
+            {
+                answer: "449964",
+                isCorrect: true
+            },
+            {
+                answer: "400964",
+                isCorrect: false
+            },
+            {
+                answer: "449250",
+                isCorrect: false
+            },
+            {
+                answer: "543962",
+                isCorrect: false
+            }
+        ],
+        country: "Sweden"
+    },
+    {
+        question: "What is the official currency of Sweden?",
+        answers: [
+            {
+                answer: "Swedish krona",
+                isCorrect: true
+            },
+            {
+                answer: "Euro",
+                isCorrect: false
+            },
+            {
+                answer: "United States dollar",
+                isCorrect: false
+            },
+            {
+                answer: "British pound",
+                isCorrect: false
+            }
+        ],
+        country: "Sweden"
+    },
+    {
+        question: "What is the population density of Sweden (people per km2)?",
+        answers: [
+            {
+                answer: "20",
+                isCorrect: true
+            },
+            {
+                answer: "10",
+                isCorrect: false
+            },
+            {
+                answer: "60",
+                isCorrect: false
+            },
+            {
+                answer: "100",
+                isCorrect: false
+            }
+        ],
+        country: "Sweden"
+    },
+    {
+        question: "What is the population of Sweden?",
+        answers: [
+            {
+                answer: "9041262",
+                isCorrect: true
+            },
+            {
+                answer: "12230543",
+                isCorrect: false
+            },
+            {
+                answer: "30980262",
+                isCorrect: false
+            },
+            {
+                answer: "7340128",
+                isCorrect: false
+            }
+        ],
+        country: "Sweden"
+    },
+    {
+        question: "What is the capital of United Kingdom?",
+        answers: [
+            {
+                answer: "London",
+                isCorrect: true
+            },
+            {
+                answer: "Madrid",
+                isCorrect: false
+            },
+            {
+                answer: "Dublin",
+                isCorrect: false
+            },
+            {
+                answer: "Paris",
+                isCorrect: false
+            }
+        ],
+        country: "United Kingdom"
+    },
+    {
+        question: "What is the official currency of United Kingdom?",
+        answers: [
+            {
+                answer: "British pound",
+                isCorrect: true
+            },
+            {
+                answer: "British lira",
+                isCorrect: false
+            },
+            {
+                answer: "Euro",
+                isCorrect: false
+            },
+            {
+                answer: "Lev",
+                isCorrect: false
+            }
+        ],
+        country: "United Kingdom"
+    },
+    {
+        question: "What is the population of United Kingdom?",
+        answers: [
+            {
+                answer: "59667840",
+                isCorrect: true
+            },
+            {
+                answer: "65250510",
+                isCorrect: false
+            },
+            {
+                answer: "50662591",
+                isCorrect: false
+            },
+            {
+                answer: "95403581",
+                isCorrect: false
+            }
+        ],
+        country: "United Kingdom"
+    },
+    {
+        question: "What is the population density of United Kingdom (people per km2)?",
+        answers: [
+            {
+                answer: "246",
+                isCorrect: true
+            },
+            {
+                answer: "98",
+                isCorrect: false
+            },
+            {
+                answer: "326",
+                isCorrect: false
+            },
+            {
+                answer: "159",
+                isCorrect: false
+            }
+        ],
+        country: "United Kingdom"
+    },
+    {
+        question: "What is the area of United Kingdom (in km2)?",
+        answers: [
+            {
+                answer: "242900",
+                isCorrect: true
+            },
+            {
+                answer: "290340",
+                isCorrect: false
+            },
+            {
+                answer: "180637",
+                isCorrect: false
+            },
+            {
+                answer: "320700",
+                isCorrect: false
+            }
+        ],
+        country: "United Kingdom"
+    },
+    {
+        question: "What is the capital of Slovenia",
+        answers: [
+            {
+                answer: "Ljubljana",
+                isCorrect: true
+            },
+            {
+                answer: "Bratislava",
+                isCorrect: false
+            },
+            {
+                answer: "Kyiv",
+                isCorrect: false
+            },
+            {
+                answer: "Belgrade",
+                isCorrect: false
+            }
+        ],
+        country: "Slovenia"
+    },
+    {
+        question: "What is the official currency of Slovenia?",
+        answers: [
+            {
+                answer: "Euro",
+                isCorrect: true
+            },
+            {
+                answer: "Slovenian krona",
+                isCorrect: false
+            },
+            {
+                answer: "British pound",
+                isCorrect: false
+            },
+            {
+                answer: "Moldovan leu",
+                isCorrect: false
+            }
+        ],
+        country: "Slovenia"
+    },
+    {
+        question: "What is the area of Slovenia (km2)?",
+        answers: [
+            {
+                answer: "20256",
+                isCorrect: true
+            },
+            {
+                answer: "10451",
+                isCorrect: false
+            },
+            {
+                answer: "25600",
+                isCorrect: false
+            },
+            {
+                answer: "32540",
+                isCorrect: false
+            }
+        ],
+        country: "Slovenia"
+    },
+    {
+        question: "What is the population of Slovenia?",
+        answers: [
+            {
+                answer: "1966814",
+                isCorrect: true
+            },
+            {
+                answer: "3004560",
+                isCorrect: false
+            },
+            {
+                answer: "10230560",
+                isCorrect: false
+            },
+            {
+                answer: "1000500",
+                isCorrect: false
+            }
+        ],
+        country: "Slovenia"
+    },
+    {
+        question: "What is the population density of Slovenia (people per km2)?",
+        answers: [
+            {
+                answer: "41",
+                isCorrect: true
+            },
+            {
+                answer: "108",
+                isCorrect: false
+            },
+            {
+                answer: "20",
+                isCorrect: false
+            },
+            {
+                answer: "<1",
+                isCorrect: false
+            }
+        ],
+        country: "Slovenia"
+    },
+    {
+        question: "What is the capital of Switzerland?",
+        answers: [
+            {
+                answer: "Bern",
+                isCorrect: true
+            },
+            {
+                answer: "Bucharest",
+                isCorrect: false
+            },
+            {
+                answer: "Kyiv",
+                isCorrect: false
+            },
+            {
+                answer: "Stockholm",
+                isCorrect: false
+            }
+        ],
+        country: "Switzerland"
+    },
+    {
+        question: "What is the official currency of Switzerland?",
+        answers: [
+            {
+                answer: "Swiss franc",
+                isCorrect: true
+            },
+            {
+                answer: "Swiss lira",
+                isCorrect: false
+            },
+            {
+                answer: "Euro",
+                isCorrect: false
+            },
+            {
+                answer: "British pound",
+                isCorrect: false
+            }
+        ],
+        country: "Switzerland"
+    },
+    {
+        question: "What is the area of Switzerland (km2)?",
+        answers: [
+            {
+                answer: "41284",
+                isCorrect: true
+            },
+            {
+                answer: "50302",
+                isCorrect: false
+            },
+            {
+                answer: "65482",
+                isCorrect: false
+            },
+            {
+                answer: "23087",
+                isCorrect: false
+            }
+        ],
+        country: "Switzerland"
+    },
+    {
+        question: "What is the population of Switzerland?",
+        answers: [
+            {
+                answer: "7252331",
+                isCorrect: true
+            },
+            {
+                answer: "10460320",
+                isCorrect: false
+            },
+            {
+                answer: "5032610",
+                isCorrect: false
+            },
+            {
+                answer: "9300406",
+                isCorrect: false
+            }
+        ],
+        country: "Switzerland"
+    },
+    {
+        question: "What is the population density of Switzerland (people per km2)?",
+        answers: [
+            {
+                answer: "176",
+                isCorrect: true
+            },
+            {
+                answer: "250",
+                isCorrect: false
+            },
+            {
+                answer: "98",
+                isCorrect: false
+            },
+            {
+                answer: "202",
+                isCorrect: false
+            }
+        ],
+        country: "Switzerland"
+    },
+    {
+        question: "What is the capital of Spain?",
+        answers: [
+            {
+                answer: "Madrid",
+                isCorrect: true
+            },
+            {
+                answer: "Lisbon",
+                isCorrect: false
+            },
+            {
+                answer: "Valletta",
+                isCorrect: false
+            },
+            {
+                answer: "Vienna",
+                isCorrect: false
+            }
+        ],
+        country: "Spain"
+    },
+    {
+        question: "What is the official currency of Spain?",
+        answers: [
+            {
+                answer: "Euro",
+                isCorrect: true
+            },
+            {
+                answer: "Spain dollar",
+                isCorrect: false
+            },
+            {
+                answer: "American dollar",
+                isCorrect: false
+            },
+            {
+                answer: "British pound",
+                isCorrect: false
+            }
+        ],
+        country: "Spain"
+    },
+    {
+        question: "What is the area of Spain (km2)?",
+        answers: [
+            {
+                answer: "505992",
+                isCorrect: true
+            },
+            {
+                answer: "600340",
+                isCorrect: false
+            },
+            {
+                answer: "150287",
+                isCorrect: false
+            },
+            {
+                answer: "403592",
+                isCorrect: false
+            }
+        ],
+        country: "Spain"
+    },
+    {
+        question: "What is the population of Spain?",
+        answers: [
+            {
+                answer: "43064190",
+                isCorrect: true
+            },
+            {
+                answer: "52450281",
+                isCorrect: false
+            },
+            {
+                answer: "48930418",
+                isCorrect: false
+            },
+            {
+                answer: "22967314",
+                isCorrect: false
+            }
+        ],
+        country: "Spain"
+    },
+    {
+        question: "What is the population density of Spain (people per km2)?",
+        answers: [
+            {
+                answer: "85",
+                isCorrect: true
+            },
+            {
+                answer: "103",
+                isCorrect: false
+            },
+            {
+                answer: "54",
+                isCorrect: false
+            },
+            {
+                answer: "197",
+                isCorrect: false
+            }
+        ],
+        country: "Spain"
+    }
+]
