9cd8a16991827d97cf956d84c85621db5dc8eceb
Nikolay Prodanow <nikolayprodanow@gmail.com>
2016-11-29 16:10:45 +0200

    refactoring

--- public/js/question-handler.js
+++ public/js/question-handler.js
@@ -1,5 +1,12 @@
 /* globals $ toastr*/
 
+function shuffle(array) {
+    for (let i = array.length; i; i -= 1) {
+        let j = Math.floor(Math.random() * i);
+        [array[i - 1], array[j]] = [array[j], array[i - 1]];
+    }
+}
+
 $("body").on("click", ".btn", (ev) => {
     let $target = $(ev.target);
     let choosedAnswer = $target.html();
@@ -14,10 +21,10 @@ $("body").on("click", ".btn", (ev) => {
             method: "POST",
             contentType: "application/json",
             data: JSON.stringify(body),
-            success: function (response) {
+            success: function(response) {
                 resolve(response);
             },
-            error: function () {
+            error: function() {
 
             }
         });
@@ -34,7 +41,7 @@ $("body").on("click", ".btn", (ev) => {
 
         let answers = response.newQuestion.answers;
         shuffle(answers);
-        
+
         let $list = $(".answers-item");
         let $currentLi = $list.eq(0);
         let count = 1;
@@ -50,10 +57,3 @@ $("body").on("click", ".btn", (ev) => {
         });
 
 });
-
-function shuffle(array) {
-    for (let i = array.length; i; i -= 1) {
-        let j = Math.floor(Math.random() * i);
-        [array[i - 1], array[j]] = [array[j], array[i - 1]];
-    }
-}
