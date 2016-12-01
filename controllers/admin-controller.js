b1c4dcd54b6ac6519860f5b302a27f12efa0b488
Nikolay Prodanow <nikolayprodanow@gmail.com>
2016-12-01 20:34:55 +0200

    add create question validator

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -19,6 +19,45 @@ module.exports = function(params) {
 
         },
         createQuestion(req, res) {
+            let validatorError = {};
+            validator.messages = [];
+
+            let question = req.body.question;
+            let answers = req.body.answers;
+            let country = req.body.country;
+
+            if (!validator.validateIsStringValid(question)) {
+                validatorError.error = true;
+                validatorError.messages.push("Question is not valid");
+            }
+
+            if (!validator.validateStringLength(question, 5, 100)) {
+                validatorError.error = true;
+                validatorError.messages.push("Question length must be between 5 and 100 characters");
+            }
+
+            if (!Array.isArray(answers)) {
+                validatorError.error = true;
+                validatorError.messages.push("Answers error");
+            } else {
+                answers.forEach((value) => {
+                    if (!validator.validateIsStringValid(value.answer)) {
+                        validatorError.error = true;
+                        validatorError.messages.push("Answer is not valid");
+                    }
+
+                    if (typeof value.isCorrect !== "boolean") {
+                        validatorError.error = true;
+                        validatorError.messages.push("Answer has no correct status");
+                    }
+                });
+            }
+
+            if (!validator.validateIsStringValid(country)) {
+                validatorError.error = true;
+                validatorError.messages.push("Country is not valid");
+            }
+
             data.createQuestion(req.body.question, req.body.answers, req.body.country)
                 .then(res.status(201).json({ "message": "Successfully created message." }))
                 .catch(err => {
