9ca60725141d7aefd934e8ca0ed28e37c02e14b7
Warez-VD <vlado547@outlook.com>
2016-12-02 14:49:41 +0200

    chat message date refactoring

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,17 @@
 /* globals io $ requester */
 
-var socket = io();
+function formatDate(date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let ampm = hours >= 12 ? "pm" : "am";
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    let strTime = `${hours}:${minutes} ${ampm}`;
+    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()} ${strTime}`;
+}
+
+let socket = io();
 $("body").on("click", "#sendMsg", () => {
     let user = $("#currentUsername").text();
     let answer = $("#m").val();
@@ -17,20 +28,22 @@ $("body").on("click", "#sendMsg", () => {
             $("#m").val("");
             return false;
         });
-
 });
 
 socket.on("chat message", (msg) => {
-    console.log(msg);
+    let messageDate = formatDate(new Date(msg.datetime));
     let container = $("#message-container");
     let messageUser = $("<p />")
         .addClass("text-muted")
         .text(msg.user);
     let messageContent = $("<span />")
-        .text(msg.datetime + " " + msg.answer);
+        .text(msg.answer);
+    let messageContentDate = $("<div />")
+        .text(`sent ${messageDate}`);
     let message = $("<div />")
         .addClass("row message-bubble");
     message.append(messageUser);
     message.append(messageContent);
+    message.append(messageContentDate);
     container.append(message);
 });
