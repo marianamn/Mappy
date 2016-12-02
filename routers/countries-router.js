5595043717ce5c24ebac86791d4239c42ee08c4e
v4ss1l <vasil.cc@abv.bg>
2016-12-02 15:35:30 +0200

    renamed analytics middleware

--- routers/countries-router.js
+++ routers/countries-router.js
@@ -1,17 +1,27 @@
 /* globals module require */
 
 const express = require("express");
+
 let Router = express.Router;
+let isAuthenticated = require("../middlewares/is-user-authenticated");
 let analytics = require("../middlewares/analytics");
 
 module.exports = function({ app, controllers }) {
     let router = new Router();
 
     router
-        .get("/", analytics, controllers.allCountries)
-        .get("/:id", analytics, controllers.countryDetails);
+        .get("/test-your-knowledge", analytics, isAuthenticated, controllers.getTestKnowledgeMap)
+        .get("/test-your-knowledge/:countryName", isAuthenticated, controllers.getQuestion);
 
-    app.use("/countries", router);
+    app.use("/game", router);
+
+    return router;
+};
+    return router;
+};      .put("/profile", isAuthenticated, controllers.updateProfile)
+        .put("/users/:username", isAuthenticated, isAdmin, controllers.updateUserRole);
+
+    app.use("/api", router);
 
     return router;
 };
