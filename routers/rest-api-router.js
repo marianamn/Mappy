261968cd20f1d6eda69ee1ac01e1c1dbdeaf99de
Nikolay Prodanow <nikolayprodanow@gmail.com>
2016-12-04 12:45:42 +0200

    add middleware in api reouters

--- routers/rest-api-router.js
+++ routers/rest-api-router.js
@@ -3,11 +3,11 @@
 const express = require("express");
 let Router = express.Router;
 
-module.exports = function({ app, controllers, middlewares }) {
+module.exports = function ({ app, controllers, middlewares }) {
     let router = new Router();
 
     router
-        .get("/users", controllers.getAllUsernames)
+        .get("/users", middlewares.isAuthenticated, controllers.getAllUsernames)
         .get("/countries", middlewares.isAuthenticated, middlewares.isAdmin, controllers.getAllCountriesNames)
         .post("/register", middlewares.analytics, controllers.register)
         .post("/createQuestion", middlewares.isAuthenticated, middlewares.isAdmin, controllers.createQuestion)
