9b771d3ebba36c67615257076bbad1bdfd187769
Nikolay Prodanow <nikolayprodanow@gmail.com>
2016-12-04 12:38:44 +0200

    add tests for home routers

---
+++ tests/routers/home-router-tests.js
@@ -0,0 +1,67 @@
+/* globals require describe it beforeEach afterEach before after*/
+
+const chai = require("chai");
+const sinonModule = require("sinon");
+const chaiHttp = require("chai-http");
+
+chai.use(chaiHttp);
+let expect = chai.expect;
+
+describe("Test home router", () => {
+    let sinon;
+
+    let controllers = {
+        home: () => { }
+    };
+
+    let middlewares = {
+        analytics: () => { }
+    };
+
+    beforeEach(() => {
+        sinon = sinonModule.sandbox.create();
+    });
+
+    afterEach(() => {
+        sinon.restore();
+    });
+
+    describe("GET /", () => {
+        it("expect to get status 200 when request home", done => {
+            sinon.stub(controllers, "home", (req, res) => {
+                res.status(200).end();
+            });
+
+            sinon.stub(middlewares, "analytics", (req, res, next) => {
+                return next();
+            });
+
+            let { app } = require("../../config/application")({ data: {} });
+            require("../../routers/home-router")({ app, controllers, middlewares });
+
+            chai.request(app)
+                .get("/")
+                .end((req, res) => {
+                    expect(res.status).equals(200);
+                    done();
+                });
+        });
+
+        it("expect to get status 200 when request terms of use", done => {
+            sinon.stub(middlewares, "analytics", (req, res, next) => {
+                req.user = { username: "test" };
+                return next();
+            });
+
+            let { app } = require("../../config/application")({ data: {} });
+            require("../../routers/home-router")({ app, controllers, middlewares });
+
+            chai.request(app)
+                .get("/terms")
+                .end((req, res) => {
+                    expect(res.status).equals(200);
+                    done();
+                });
+        });
+    });
+});
